aa441a33e8f3e3fad97a08e23280754e6439af4e
Renan Couto <rennanccouto@gmail.com>
2022-01-06 12:11:48 -0300

    Primeiro Commit

---
+++ src/main/java/com/renan/ApiPacienteYoux/ApiPacienteYouxApplication.java
@@ -0,0 +1,13 @@
+package com.renan.ApiPacienteYoux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPacienteYouxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPacienteYouxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/ApiPacienteYoux/ApiPacienteYouxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.ApiPacienteYoux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPacienteYouxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
